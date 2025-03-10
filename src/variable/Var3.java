e4748813fc5fa6b91575e41e2a24b967c179c48d
bestdevmgp <pmg3858@icloud.com>
2025-03-10 12:43:23 +0900

    Initial commit

---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a = 100;                // 정수
+        double b = 10.5;            // 실수
+        boolean c = true;           // 참과 거짓을 판단할 때 사용 true, false 입력 가능
+        char d = 'A';               // 문자 하나, 작은 따옴표를 사용
+        String e = "Hello Java";    // 문자열, 큰 따옴표를 사용
+    }
+}
---
+++ src/variable/Var4.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        // 정수
+        byte e = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+
+        // 실수
+        float f = 3.141592653589793f;
+        double d = 3.14159265358979d;
+
+        // 기타
+        boolean b = true;
+        char c = 'c';
+        String str = "abc";
+    }
+}
