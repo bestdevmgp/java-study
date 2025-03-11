3ecacd06b4e7deed639319b68913480892e90c17
bestdevmgp <pmg3858@icloud.com>
2025-03-11 11:42:27 +0900

    Operation

---
+++ src/operation/OperationEx3.java
@@ -0,0 +1,8 @@
+package operation;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 75;
+        System.out.println(score >= 80 && score <= 100);
+    }
+}
