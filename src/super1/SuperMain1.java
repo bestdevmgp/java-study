6b45f542c6542eb9a8ff95ec2597c2cd294be19f
bestdevmgp <pmg3858@icloud.com>
2025-04-16 10:41:41 +0900

    super

---
+++ src/super1/SuperMain1.java
@@ -0,0 +1,10 @@
+package super1;
+
+import access.Child;
+
+public class SuperMain1 {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
