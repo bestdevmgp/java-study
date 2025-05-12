87aca8209b8c78a8dde9667f80278ab276f64c02
bestdevmgp <im@parkmingyu.com>
2025-05-12 20:41:00 +0900

    anonymous

---
+++ src/nested/inner/ex1/CarMain.java
@@ -0,0 +1,7 @@
+package nested.inner.ex1;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car myCar = new Car("Model Y", 100);
+    }
+}
---
+++ src/nested/nested/annoymous/ex/Process.java
@@ -0,0 +1,5 @@
+package nested.nested.annoymous.ex;
+
+public interface Process {
+    void run();
+}
